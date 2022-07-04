6ed4eb6769ad4a03ff46dd4e5d5ddd85fb4b4872
Peter Blackburn <peteblackburn@hotmail.com>
2022-07-04 17:16:46 +0100

    c

--- pytest/testcode1.py
+++ pytest/testcode1.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sys
+
 def main():
-    print('hello world')
+    print('hello world, using python version: '+str(sys.version_info[0]))
 
 if __name__ == '__main__':
     main()
